ada577658e2427a37f398bc5a381efc092d0dd04
thomas <thomas@irstv>
2008-06-13 09:26:29 +0000

    update of each Java source file header (relative to license message)

--- src/test/java/org/h2spatial/AllTests.java
+++ src/test/java/org/h2spatial/AllTests.java
@@ -1,36 +1,33 @@
 /*
- * The GRAP library (GeoRAster Processing) is a middleware dedicated
- * to the processing of various kinds of geographic raster data. It
- * provides a complete and robust API to manipulate ASCII Grid or
- * tiff, png, bmp, jpg (with the corresponding world file) geographic
- * images. GRAP is produced  by the geomatic team of the IRSTV Institute
- * <http://www.irstv.cnrs.fr/>, CNRS FR 2488:
+ * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
+ * This cross-platform GIS is developed at French IRSTV institute and is able
+ * to manipulate and create vector and raster spatial information. OrbisGIS
+ * is distributed under GPL 3 license. It is produced  by the geo-informatic team of
+ * the IRSTV Institute <http://www.irstv.cnrs.fr/>, CNRS FR 2488:
  *    Erwan BOCHER, scientific researcher,
  *    Thomas LEDUC, scientific researcher,
  *    Fernando GONZALEZ CORTES, computer engineer.
  *
  * Copyright (C) 2007 Erwan BOCHER, Fernando GONZALEZ CORTES, Thomas LEDUC
  *
- * This file is part of GRAP.
+ * This file is part of OrbisGIS.
  *
- * GRAP is free software: you can redistribute it and/or modify
+ * OrbisGIS is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * GRAP is distributed in the hope that it will be useful,
+ * OrbisGIS is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with GRAP. If not, see <http://www.gnu.org/licenses/>.
+ * along with OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
  *
  * For more information, please consult:
  *    <http://orbisgis.cerma.archi.fr/>
  *    <http://sourcesup.cru.fr/projects/orbisgis/>
- *    <http://listes.cru.fr/sympa/info/orbisgis-developers/>
- *    <http://listes.cru.fr/sympa/info/orbisgis-users/>
  *
  * or contact directly:
  *    erwan.bocher _at_ ec-nantes.fr
