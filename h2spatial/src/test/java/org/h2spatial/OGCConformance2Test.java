12ebc024a7e64d2e0f139834ae85010e8b6e66a7
nicolas-f <github@nettrader.fr>
2013-04-29 17:33:42 +0200

    add conformance OGC 2 Did not pass, cause hexadecimal conversion

--- h2spatial/src/test/java/org/h2spatial/OGCConformance2Test.java
+++ h2spatial/src/test/java/org/h2spatial/OGCConformance2Test.java
@@ -25,8 +25,169 @@
 
 package org.h2spatial;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * @author Nicolas Fortin
  */
 public class OGCConformance2Test {
+    private static final String DB_FILE_PATH = "target/test-resources/dbH2_OGC_Conf2";
+    private static final File DB_FILE = new File(DB_FILE_PATH+".h2.db");
+    private static final String DATABASE_PATH = "jdbc:h2:"+DB_FILE_PATH;
+    private static Connection connection;
+
+    @BeforeClass
+    public static void tearUp() throws Exception {
+        Class.forName("org.h2.Driver");
+        if(DB_FILE.exists()) {
+            DB_FILE.delete();
+        }
+        // Keep a connection alive to not close the DataBase on each unit test
+        connection = DriverManager.getConnection(DATABASE_PATH,
+                "sa", "");
+        // Set up test data
+        URL sqlURL = OGCConformance1Test.class.getResource("ogc_conformance_test2.sql");
+        Statement st = connection.createStatement();
+        st.execute("RUNSCRIPT FROM '"+sqlURL+"'");
+    }
+
+
+    /**
+     *  For this test, we will check to see that all of the feature tables are
+     *  represented by entries in the GEOMETRY_COLUMNS table/view.
+     *  @throws Exception
+     */
+    @Test
+    public void N1() throws Exception {
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT f_table_name FROM geometry_columns;");
+        Set<String> tablesWithGeometry = new HashSet<String>(11);
+        while(rs.next()) {
+            tablesWithGeometry.add(rs.getString("f_table_name"));
+        }
+        assertTrue(tablesWithGeometry.contains("lakes"));
+        assertTrue(tablesWithGeometry.contains("road_segments"));
+        assertTrue(tablesWithGeometry.contains("divided_routes"));
+        assertTrue(tablesWithGeometry.contains("buildings"));
+        assertTrue(tablesWithGeometry.contains("forests"));
+        assertTrue(tablesWithGeometry.contains("bridges"));
+        assertTrue(tablesWithGeometry.contains("named_places"));
+        assertTrue(tablesWithGeometry.contains("streams"));
+        assertTrue(tablesWithGeometry.contains("ponds"));
+        assertTrue(tablesWithGeometry.contains("map_neatlines"));
+    }
+
+    /**
+     * For this test, we will check to see that all of the geometry tables are
+     * represented by entries in the GEOMETRY_COLUMNS table/view.
+     * @throws Exception
+     */
+    @Test
+    public void N2() throws Exception {
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT g_table_name FROM geometry_columns;");
+        Set<String> tablesWithGeometry = new HashSet<String>(11);
+        while(rs.next()) {
+            tablesWithGeometry.add(rs.getString("g_table_name"));
+        }
+        assertTrue(tablesWithGeometry.contains("lake_geom"));
+        assertTrue(tablesWithGeometry.contains("road_segment_geom"));
+        assertTrue(tablesWithGeometry.contains("divided_route_geom"));
+        assertTrue(tablesWithGeometry.contains("forest_geom"));
+        assertTrue(tablesWithGeometry.contains("bridge_geom"));
+        assertTrue(tablesWithGeometry.contains("stream_geom"));
+        assertTrue(tablesWithGeometry.contains("building_pt_geom"));
+        assertTrue(tablesWithGeometry.contains("building_area_geom"));
+        assertTrue(tablesWithGeometry.contains("pond_geom"));
+        assertTrue(tablesWithGeometry.contains("named_place_geom"));
+        assertTrue(tablesWithGeometry.contains("map_neatline_geom"));
+    }
+
+    /**
+     * For this test, we will check to see that the correct storage type for
+     * the streams table is represented in the GEOMETRY_COLUMNS table/view.
+     * @throws Exception
+     */
+    @Test
+    public void N3() throws Exception {
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT storage_type FROM geometry_columns WHERE f_table_name = 'streams';");
+        assertTrue(rs.next());
+        assertEquals(1, rs.getInt(1));
+    }
+
+    /**
+     * For this test, we will check to see that the correct geometry type for
+     * the streams table is represented in the GEOMETRY_COLUMNS table/view.
+     * @throws Exception
+     */
+    @Test
+    public void N4() throws Exception {
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT geometry_type FROM geometry_columns WHERE f_table_name = 'streams';");
+        assertTrue(rs.next());
+        assertEquals(3, rs.getInt(1));
+    }
+
+    /**
+     * For this test, we will check to see that the correct coordinate dimension for
+     * the streams table is represented in the GEOMETRY_COLUMNS table/view.
+     * @throws Exception
+     */
+    @Test
+    public void N5() throws Exception {
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT coord_dimension FROM geometry_columns WHERE f_table_name = 'streams';");
+        assertTrue(rs.next());
+        assertEquals(2, rs.getInt(1));
+    }
+
+    /**
+     * For this test, we will check to see that the correct value of srid for
+     * the streams table is represented in the GEOMETRY_COLUMNS table/view.
+     * @throws Exception
+     */
+    @Test
+    public void N6() throws Exception {
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT srid FROM geometry_columns WHERE f_table_name = 'streams';");
+        assertTrue(rs.next());
+        assertEquals(101, rs.getInt(1));
+    }
+
+    /**
+     * For this test, we will check to see that the correct value of srtext is
+     * represented in the SPATIAL_REF_SYS table/view.
+     * @throws Exception
+     */
+    @Test
+    public void N7() throws Exception {
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT srtext FROM SPATIAL_REF_SYS WHERE SRID = 101;");
+        assertTrue(rs.next());
+        assertEquals("PROJCS[\"UTM_ZONE_14N\", GEOGCS[\"World Geodetic System\n\n72\",DATUM[\"WGS_72\", " +
+                "ELLIPSOID[\"NWL_10D\", 6378135,\n\n298.26]],PRIMEM[\"Greenwich\",\n\n0],UNIT[\"Meter\",1.0]]," +
+                "PROJECTION[\"Transverse_Mercator\"],\n\nPARAMETER[\"False_Easting\", 500000.0]," +
+                "PARAMETER[\"False_Northing\",\n\n0.0],PARAMETER[\"Central_Meridian\", -99.0],PARAMETER[\"Scale_Factor\"" +
+                ",\n\n0.9996],PARAMETER[\"Latitude_of_origin\", 0.0],UNIT[\"Meter\", 1.0]]", rs.getString(1));
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        connection.close();
+    }
 }
