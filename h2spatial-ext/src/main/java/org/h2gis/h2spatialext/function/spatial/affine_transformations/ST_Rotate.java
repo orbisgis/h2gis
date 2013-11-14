6ae95784ff22f5f7e30ac303707de0ebcaf8a5a5
Adam Gouge <adam.gouge@ec-nantes.fr>
2013-11-14 15:03:23 +0100

    Fix some naming problems from copy-paste.

--- h2spatial-ext/src/main/java/org/h2gis/h2spatialext/function/spatial/affine_transformations/ST_Rotate.java
+++ h2spatial-ext/src/main/java/org/h2gis/h2spatialext/function/spatial/affine_transformations/ST_Rotate.java
@@ -46,7 +46,7 @@ public class ST_Rotate extends DeterministicScalarFunction {
 
     @Override
     public String getJavaStaticMethod() {
-        return "getMaxX";
+        return "rotate";
     }
 
     /**
@@ -57,9 +57,9 @@ public class ST_Rotate extends DeterministicScalarFunction {
      * @param theta Angle
      * @return The geometry rotated about the center of its envelope
      */
-    public static Geometry getMaxX(Geometry geom, double theta) {
+    public static Geometry rotate(Geometry geom, double theta) {
         Coordinate center = geom.getEnvelopeInternal().centre();
-        return getMaxX(geom, theta, center.x, center.y);
+        return rotate(geom, theta, center.x, center.y);
     }
 
     /**
@@ -71,8 +71,8 @@ public class ST_Rotate extends DeterministicScalarFunction {
      * @param point The point about which to rotate
      * @return The geometry rotated by theta about the given point
      */
-    public static Geometry getMaxX(Geometry geom, double theta, Point point) {
-        return getMaxX(geom, theta, point.getX(), point.getY());
+    public static Geometry rotate(Geometry geom, double theta, Point point) {
+        return rotate(geom, theta, point.getX(), point.getY());
     }
 
     /**
@@ -85,7 +85,7 @@ public class ST_Rotate extends DeterministicScalarFunction {
      * @param y0    y-coordinate of point about which to rotate
      * @return The geometry rotated by theta about (x0, y0)
      */
-    public static Geometry getMaxX(Geometry geom, double theta, double x0, double y0) {
+    public static Geometry rotate(Geometry geom, double theta, double x0, double y0) {
         if (geom != null) {
             return AffineTransformation.rotationInstance(theta, x0, y0).transform(geom);
         } else {
