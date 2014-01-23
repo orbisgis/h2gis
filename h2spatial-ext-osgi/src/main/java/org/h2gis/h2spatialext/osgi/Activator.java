399f72eccefc03dcf3159545822d03a83dea1093
Erwan Bocher <erwan.bocher@ec-nantes.fr>
2014-01-23 16:11:11 +0100

    Load geojson driver

--- h2spatial-ext-osgi/src/main/java/org/h2gis/h2spatialext/osgi/Activator.java
+++ h2spatial-ext-osgi/src/main/java/org/h2gis/h2spatialext/osgi/Activator.java
@@ -1,6 +1,7 @@
 package org.h2gis.h2spatialext.osgi;
 
 import org.h2gis.drivers.dbf.DBFDriverFunction;
+import org.h2gis.drivers.geojson.GeoJsonDriverFunction;
 import org.h2gis.drivers.gpx.GPXDriverFunction;
 import org.h2gis.drivers.shp.SHPDriverFunction;
 import org.h2gis.h2spatialapi.DriverFunction;
@@ -33,6 +34,7 @@ public class Activator implements BundleActivator {
                 bc.registerService(DriverFunction.class, new DBFDriverFunction(), null);
                 bc.registerService(DriverFunction.class, new SHPDriverFunction(), null);
                 bc.registerService(DriverFunction.class, new GPXDriverFunction(), null);
+                bc.registerService(DriverFunction.class, new GeoJsonDriverFunction(), null);
         }
 
         /**
