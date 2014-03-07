dcbedd68e2267feb3c00c33bbfc668c476313afe
nicolas-f <github@nettrader.fr>
2014-03-07 11:36:41 +0100

    Fix DBF unit test

--- h2drivers/src/test/java/org/h2gis/drivers/dbf/DBFEngineTest.java
+++ h2drivers/src/test/java/org/h2gis/drivers/dbf/DBFEngineTest.java
@@ -55,6 +55,7 @@ public class  DBFEngineTest {
         connection = SpatialH2UT.createSpatialDataBase(DB_NAME);
         CreateSpatialExtension.registerFunction(connection.createStatement(), new DriverManager(), "");
         CreateSpatialExtension.registerFunction(connection.createStatement(), new DBFRead(), "");
+        CreateSpatialExtension.registerFunction(connection.createStatement(), new DBFWrite(), "");
     }
 
     @AfterClass
@@ -191,6 +192,10 @@ public class  DBFEngineTest {
         st.execute("drop table if exists dbftable");
     }
 
+    /**
+     * Read a DBF where the encoding is missing in header.
+     * @throws SQLException
+     */
     @Test
     public void readDBFRussianEncodingTest() throws SQLException {
         Statement st = connection.createStatement();
@@ -222,8 +227,39 @@ public class  DBFEngineTest {
     }
 
 
-
+    /**
+     * Read a DBF where the encoding is missing in header. Then write it with a good header. Then check the content.
+     * @throws SQLException
+     */
     @Test
-    public void readDBFRussianWrongEncodingThenWriteThenRead() throws SQLException
+    public void readDBFRussianWrongEncodingThenWriteThenRead() throws SQLException {
+        Statement st = connection.createStatement();
+        st.execute("drop table if exists sotchi");
+        st.execute("CALL DBFREAD("+StringUtils.quoteStringSQL(DBFEngineTest.class.getResource("sotchi.dbf").getPath())+", 'SOTCHI', 'cp1251');");
+        st.execute("CALL DBFWRITE('target/sotchi.dbf', 'SOTCHI', 'cp1251');");
+        st.execute("drop table if exists sotchi");
+        st.execute("CALL FILE_TABLE('target/sotchi.dbf', 'SOTCHI_GOODHEADER');");
+        ResultSet rs = st.executeQuery("SELECT * FROM SOTCHI_GOODHEADER");
+        // Check if fields name are OK
+        ResultSetMetaData meta = rs.getMetaData();
+        assertEquals("B_ДНА",meta.getColumnName(4));
+        assertEquals("ИМЕНА_УЧАС",meta.getColumnName(7));
+        assertEquals("ДЛИНА_КАНА",meta.getColumnName(8));
+        assertEquals("ДЛИНА_КАН_",meta.getColumnName(9));
+        assertEquals("ИМЯ_МУООС",meta.getColumnName(10));
+        assertTrue(rs.next());
+        assertEquals("ВП-2", rs.getString("NAMESHEME"));
+        assertEquals("Дубовский канал",rs.getString("NAME10000"));
+        assertTrue(rs.next());
+        assertEquals("ВП-2-кр1-2", rs.getString("NAMESHEME"));
+        assertTrue(rs.next());
+        assertEquals("ВП-1", rs.getString("NAMESHEME"));
+        assertTrue(rs.next());
+        assertEquals("ВП-2-кр1-4", rs.getString("NAMESHEME"));
+        assertTrue(rs.next());
+        assertEquals("ВП-2-кр1-4-8", rs.getString("NAMESHEME"));
+        assertFalse(rs.next());
+        rs.close();
+        st.execute("drop table SOTCHI_GOODHEADER");
     }
 }
