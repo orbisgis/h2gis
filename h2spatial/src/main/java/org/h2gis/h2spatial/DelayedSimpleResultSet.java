ad19d95d5762b5ef53b720f6729c9e819e070611
Adam Gouge <adam.gouge@ec-nantes.fr>
2014-03-31 15:23:29 +0200

    Add DelayedSimpleResultSet

---
+++ h2spatial/src/main/java/org/h2gis/h2spatial/DelayedSimpleResultSet.java
@@ -0,0 +1,30 @@
+package org.h2gis.h2spatial;
+
+import org.h2.tools.SimpleResultSet;
+
+import java.sql.SQLException;
+
+/**
+ * A {@link org.h2.tools.SimpleResultSet} where the initialization can be delayed.
+ *
+ * See https://groups.google.com/forum/#!topic/h2-database/NHH0rDeU258
+ * @author Adam Gouge
+ */
+public abstract class DelayedSimpleResultSet extends SimpleResultSet {
+
+    private boolean isInitialized = false;
+
+    @Override
+    public boolean next() throws SQLException {
+        if (!isInitialized) {
+            init();
+            isInitialized = true;
+        }
+        return super.next();
+    }
+
+    /**
+     * Initialize the ResultSet.
+     */
+    protected abstract void init() throws SQLException;
+}
