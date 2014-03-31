8467dc49f9bdf1e36c94c41cdda0a6f0e69ee944
Erwan Bocher <erwan.bocher@ec-nantes.fr>
2014-03-31 15:44:38 +0200

    Fix header

--- h2spatial-ext/src/test/java/org/h2gis/h2spatialext/SpatialFunctionTest.java
+++ h2spatial-ext/src/test/java/org/h2gis/h2spatialext/SpatialFunctionTest.java
@@ -1,8 +1,8 @@
 /**
  * h2spatial is a library that brings spatial support to the H2 Java database.
  *
- * h2spatial is distributed under GPL 3 license. It is produced by the "Atelier
- * SIG" team of the IRSTV Institute <http://www.irstv.fr/> CNRS FR 2488.
+ * h2spatial is distributed under GPL 3 license. It is produced by the "Atelier SIG"
+ * team of the IRSTV Institute <http://www.irstv.fr/> CNRS FR 2488.
  *
  * Copyright (C) 2007-2014 IRSTV (FR CNRS 2488)
  *
@@ -19,7 +19,8 @@
  * h2spatial. If not, see <http://www.gnu.org/licenses/>.
  *
  * For more information, please consult: <http://www.orbisgis.org/>
- * or contact directly: info_at_ orbisgis.org
+ * or contact directly:
+ * info_at_ orbisgis.org
  */
 package org.h2gis.h2spatialext;
 
