b6fd41f6838b631be7dcdaae252d94de21ac6ba6
Adam Gouge <adam.gouge@ec-nantes.fr>
2014-05-14 12:02:29 +0200

    Add RO paths in comment

--- h2network/src/test/java/org/h2gis/network/graph_creator/ST_ShortestPathTest.java
+++ h2network/src/test/java/org/h2gis/network/graph_creator/ST_ShortestPathTest.java
@@ -273,6 +273,14 @@ public class ST_ShortestPathTest {
 
     @Test
     public void oneToOneRO() throws Exception {
+        // | 1                | 2         | 3       | 4       | 5                |
+        // |------------------|-----------|---------|---------|------------------|
+        // | *                | (1,5,3,2) | (1,5,3) | (1,5,4) | (1,5)            |
+        // | (2,1)            | *         | (2,3)   | (2,4)   | (2,1,5), (2,4,5) |
+        // | (3,1)            | (3,2)     | *       | (3,2,4) | (3,1,5)          |
+        // | (4,3,1), (4,5,1) | (4,3,2)   | (4,3)   | *       | (4,5)            |
+        // | (5,1)            | (5,3,2)   | (5,3)   | (5,4)   | *                |
+        //
         // SELECT * FROM ST_ShortestPath('CORMEN_EDGES_ALL',
         //     'reversed - edge_orientation', i, j)
         check(oneToOne(RO, 1, 1), EMPTY);
