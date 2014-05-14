20ef87a85f27811138102a7dd3f7816b5da3a77f
Adam Gouge <adam.gouge@ec-nantes.fr>
2014-05-14 12:04:57 +0200

    Add WRO paths in comment

--- h2network/src/test/java/org/h2gis/network/graph_creator/ST_ShortestPathTest.java
+++ h2network/src/test/java/org/h2gis/network/graph_creator/ST_ShortestPathTest.java
@@ -368,6 +368,14 @@ public class ST_ShortestPathTest {
 
     @Test
     public void oneToOneWRO() throws Exception {
+        // | 1                  | 2         | 3       | 4       | 5         |
+        // |--------------------|-----------|---------|---------|-----------|
+        // | *                  | (1,5,3,2) | (1,5,3) | (1,5,4) | (1,5)     |
+        // | (2,3,1)            | *         | (2,3)   | (2,4)   | (2,4,5)   |
+        // | (3,1)              | (3,2)     | *       | (3,2,4) | (3,2,4,5) |
+        // | (4,5,1), (4,5,3,1) | (4,5,3,2) | (4,5,3) | *       | (4,5)     |
+        // | (5,1), (5,3,1)     | (5,3,2)   | (5,3)   | (5,4)   | *         |
+        //
         // SELECT * FROM ST_ShortestPath('CORMEN_EDGES_ALL',
         //     'reversed - edge_orientation', 'weight', i, j)
         check(oneToOne(RO, W, 1, 1), EMPTY);
