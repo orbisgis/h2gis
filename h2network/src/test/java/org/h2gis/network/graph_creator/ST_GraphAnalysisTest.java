a7082f94ff55396a308b7ec758d14c34098fb663
Adam Gouge <adam.gouge@ec-nantes.fr>
2014-05-15 11:54:10 +0200

    Check RO edge centrality

--- h2network/src/test/java/org/h2gis/network/graph_creator/ST_GraphAnalysisTest.java
+++ h2network/src/test/java/org/h2gis/network/graph_creator/ST_GraphAnalysisTest.java
@@ -208,6 +208,19 @@ public class ST_GraphAnalysisTest {
                         4.0 / (1.0 + 2.0 + 1.0 + 1.0 + 0.0)},
                 new double[]{1./3, 1./6, 1., 0., 1.}
         );
+        //   1: 1+1/2 = 3/2
+        //   2: 2+1/2 = 5/2
+        //   3:       = 5
+        //   4:       = 1
+        //   5: 2+1/2 = 5/2
+        //   6: 2+1/2 = 5/2
+        //   7:       = 4
+        //   8:       = 2
+        //   9: 1+2/2 = 2
+        //  10: 5+1/2 = 11/2
+        // -10: 1+1/2 = 3/2
+        final ResultSet edgeCent = st.executeQuery("SELECT * FROM CORMEN_EDGES_ALL" + EDGE_CENT_SUFFIX);
+        checkEdges(edgeCent, new double[]{1./9, 1./3, 8./9, 0., 1./3, 1./3, 2./3, 2./9, 2./9, 1., 1./9});
     }
 
     @Test
