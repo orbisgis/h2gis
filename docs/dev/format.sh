80de096e44daa90a46c95df8f32f0644bbddcebf
Adam Gouge <adam.gouge@ec-nantes.fr>
2014-05-21 15:44:14 +0200

    Clarify script

--- docs/dev/format.sh
+++ docs/dev/format.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 # Capitalize SQL types
-find . -type f -name 'ST*.md' -exec sed -i 's/(int /(INT /g' {} \;
-find . -type f -name 'ST*.md' -exec sed -i 's/ int / INT /g' {} \;
-find . -type f -name 'ST*.md' -exec sed -i 's/int ST/INT ST/g' {} \;
-find . -type f -name 'ST*.md' -exec sed -i 's/(double /(DOUBLE /g' {} \;
-find . -type f -name 'ST*.md' -exec sed -i 's/ double / DOUBLE /g' {} \;
-find . -type f -name 'ST*.md' -exec sed -i 's/double ST/DOUBLE ST/g' {} \;
-find . -type f -name 'ST*.md' -exec sed -i 's/(varchar /(VARCHAR /g' {} \;
-find . -type f -name 'ST*.md' -exec sed -i 's/ varchar / VARCHAR /g' {} \;
-find . -type f -name 'ST*.md' -exec sed -i 's/varchar ST/VARCHAR ST/g' {} \;
-find . -type f -name 'ST*.md' -exec sed -i 's/(boolean /(BOOLEAN /g' {} \;
-find . -type f -name 'ST*.md' -exec sed -i 's/ boolean / BOOLEAN /g' {} \;
-find . -type f -name 'ST*.md' -exec sed -i 's/boolean ST/BOOLEAN ST/g' {} \;
+sql_type=(int double boolean varchar)
+for f in $(find . -type f -name 'ST*.md'); do
+    for name in ${sql_type[*]}; do
+        upper_name=${name^^}
+        sed -i "s/($name /($upper_name /g" $f
+        sed -i "s/ $name / $upper_name /g" $f
+        sed -i "s/$name ST/$upper_name ST/g" $f
+    done
+done
 # Put spaces after commas
 find . -type f -name 'ST*.md' -exec sed -i 's/\([^,]*\),\([^ ]\)/\1, \2/g' {} \;
 # Remove trailing whitespace
