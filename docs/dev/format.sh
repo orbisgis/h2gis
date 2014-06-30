c3f29c4f58e31f7b7d91cc74724bf44c7159ba53
Adam Gouge <adam.gouge@ec-nantes.fr>
2014-06-30 16:52:01 +0200

    Capitalize GEOMETRY

--- docs/dev/format.sh
+++ docs/dev/format.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Remove trailing whitespace
+find . -type f -name 'ST*.md' -exec sed -i 's/[[:space:]]*$//' {} \;
+# Put spaces after commas
+find . -type f -name 'ST*.md' -exec sed -i 's/\([^,]*\),\([^ ]\)/\1, \2/g' {} \;
 # Capitalize SQL types
-sql_type=(int double boolean varchar)
+sql_type=(int double boolean varchar geometry)
 for f in $(find . -type f -name 'ST*.md'); do
     for name in ${sql_type[*]}; do
         upper_name=${name^^}
-        sed -i "s/($name /($upper_name /g" $f
-        sed -i "s/ $name / $upper_name /g" $f
-        sed -i "s/$name ST/$upper_name ST/g" $f
+        # Return type
+        sed -i "s/$name ST_/$upper_name ST_/gi" $f
+        # First parameter
+        sed -i "s/($name /($upper_name /gi" $f
+        # Any other parameter
+        sed -i "s/, $name /, $upper_name /gi" $f
     done
 done
-# Put spaces after commas
-find . -type f -name 'ST*.md' -exec sed -i 's/\([^,]*\),\([^ ]\)/\1, \2/g' {} \;
-# Remove trailing whitespace
-find . -type f -name 'ST*.md' -exec sed -i 's/[[:space:]]*$//' {} \;
