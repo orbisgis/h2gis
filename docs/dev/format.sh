774ad4f2a6c41818ea8c37b0f6621576375c8de7
Adam Gouge <adam.gouge@ec-nantes.fr>
2014-06-30 16:49:11 +0200

    Capitalize BOOLEAN

--- docs/dev/format.sh
+++ docs/dev/format.sh
@@ -9,3 +9,6 @@ find . -type f -name 'ST*.md' -exec sed -i 's/double ST/DOUBLE ST/g' {} \;
 find . -type f -name 'ST*.md' -exec sed -i 's/(varchar /(VARCHAR /g' {} \;
 find . -type f -name 'ST*.md' -exec sed -i 's/ varchar / VARCHAR /g' {} \;
 find . -type f -name 'ST*.md' -exec sed -i 's/varchar ST/VARCHAR ST/g' {} \;
+find . -type f -name 'ST*.md' -exec sed -i 's/(boolean /(BOOLEAN /g' {} \;
+find . -type f -name 'ST*.md' -exec sed -i 's/ boolean / BOOLEAN /g' {} \;
+find . -type f -name 'ST*.md' -exec sed -i 's/boolean ST/BOOLEAN ST/g' {} \;
