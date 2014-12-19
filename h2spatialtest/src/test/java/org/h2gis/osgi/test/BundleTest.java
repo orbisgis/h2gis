d6288dbdccd7161f8d2d17235fa023cbeac721c5
nicolas-f <github@nettrader.fr>
2014-12-19 09:15:37 +0100

    Switch to 1.4.184

--- h2spatialtest/src/test/java/org/h2gis/osgi/test/BundleTest.java
+++ h2spatialtest/src/test/java/org/h2gis/osgi/test/BundleTest.java
@@ -75,7 +75,7 @@ public class BundleTest {
                 mavenBundle("org.orbisgis", "cts").version("1.3.3"),
                 mavenBundle("org.orbisgis", "jts"),
                 mavenBundle("org.orbisgis", "jdelaunay"),
-                mavenBundle("com.h2database", "h2").version("1.4.183"),
+                mavenBundle("com.h2database", "h2").version("1.4.184"),
                 mavenBundle("com.fasterxml.jackson.core", "jackson-core").version("2.3.1"),
                 mavenBundle("org.orbisgis", "h2spatial").noStart(),
                 mavenBundle("org.orbisgis", "h2spatial-ext").noStart(),
