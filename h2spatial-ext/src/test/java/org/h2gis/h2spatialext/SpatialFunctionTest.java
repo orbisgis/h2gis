880ed684e347b0df0d0ec2376d0477cf22caf800
Erwan Bocher <erwan.bocher@ec-nantes.fr>
2015-02-16 19:06:34 +0100

    st_execute it is enough ;-)

--- h2spatial-ext/src/test/java/org/h2gis/h2spatialext/SpatialFunctionTest.java
+++ h2spatial-ext/src/test/java/org/h2gis/h2spatialext/SpatialFunctionTest.java
@@ -147,7 +147,7 @@ public class SpatialFunctionTest {
                     + " boundary MULTIPOLYGON);"
                     + "INSERT INTO forests VALUES(109, 'Green Forest', ST_MPolyFromText( 'MULTIPOLYGON(((28 26,28 0,84 0,"
                     + "84 42,28 26), (52 18,66 23,73 9,48 6,52 18)),((59 18,67 18,67 13,59 13,59 18)))', 101));");
-            ResultSet rs = st.executeQuery("SELECT ST_AsText(boundary) FROM ST_Explode('forests', 'the_geom') WHERE name = 'Green Forest' and explod_id=2");
+            st.execute("SELECT ST_AsText(boundary) FROM ST_Explode('forests', 'the_geom') WHERE name = 'Green Forest' and explod_id=2");
         } catch (JdbcSQLException e) {
             throw e.getOriginalCause();
         } finally {
