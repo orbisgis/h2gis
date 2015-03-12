a036f88af2426cb3892e0a77abd949fdbcdcea46
nicolas-f <github@nettrader.fr>
2015-03-12 17:32:39 +0100

    Doc

--- h2spatial-ext/src/main/java/org/h2gis/h2spatialext/function/spatial/mesh/ST_Voronoi.java
+++ h2spatial-ext/src/main/java/org/h2gis/h2spatialext/function/spatial/mesh/ST_Voronoi.java
@@ -20,10 +20,13 @@ public class ST_Voronoi extends DeterministicScalarFunction {
     private static final int DEFAULT_DIMENSION = 2;
 
     public ST_Voronoi() {
-        addProperty(PROP_REMARKS, "Construct a voronoi diagram from a delaunay triangulation.\n" +
+        addProperty(PROP_REMARKS, "Construct a voronoi diagram from a delaunay triangulation or a set of points.\n" +
                 "ST_VORONOI(THE_GEOM MULTIPOLYGON)\n" +
                 "ST_VORONOI(THE_GEOM MULTIPOLYGON,OUT_DIMENSION INTEGER)\n" +
                 "ST_VORONOI(THE_GEOM MULTIPOLYGON,OUT_DIMENSION INTEGER,ENVELOPE POLYGON)\n" +
+                "ST_VORONOI(THE_GEOM MULTIPOINTS)\n" +
+                "ST_VORONOI(THE_GEOM MULTIPOINTS,OUT_DIMENSION INTEGER)\n" +
+                "ST_VORONOI(THE_GEOM MULTIPOINTS,OUT_DIMENSION INTEGER,ENVELOPE POLYGON)\n" +
                 "Ex:\n" +
                 "SELECT ST_VORONOI(ST_DELAUNAY('MULTIPOINT(2 2 0,6 3 0,4 7 0,2 8 0,1 6 0,3 5 0)')) the_geom;\n" +
                 "SELECT ST_VORONOI(ST_DELAUNAY('MULTIPOINT(2 2 0,6 3 0,4 7 0,2 8 0,1 6 0,3 5 0)'), 1)\n" +
