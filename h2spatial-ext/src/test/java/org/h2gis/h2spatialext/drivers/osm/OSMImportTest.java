0aaeabe5d9e1b96da59b636275d0dfe3d46ab728
obonin <obonin@peterson>
2015-03-28 12:17:06 +0100

    Initial commit of the accumulation of a dead weight in shortest path algorithms.

--- h2spatial-ext/src/test/java/org/h2gis/h2spatialext/drivers/osm/OSMImportTest.java
+++ h2spatial-ext/src/test/java/org/h2gis/h2spatialext/drivers/osm/OSMImportTest.java
@@ -250,7 +250,7 @@ public class OSMImportTest {
         // Check number
         rs = st.executeQuery("SELECT count(ID_NODE) FROM OSM_NODE");
         rs.next();
-        assertEquals(3243, rs.getInt(1));
+        assertEquals(3245, rs.getInt(1));
         rs.close();
         }
     }
