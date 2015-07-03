1fdb90dba88a769426615fb5c2ee8222fad6f878
nicolas-f <github@nettrader.fr>
2015-07-03 11:22:13 +0200

    Add automatic travis build of OrbisGIS

--- travis-build.sh
+++ travis-build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 mvn install -P osgi
+# Build H2GIS dependent projects
+# Get last child project build number
+BUILD_NUM=$(curl -s 'https://api.travis-ci.org/repos/orbisgis/H2GIS/builds' | grep -o '^\[{"id":[0-9]*,' | grep -o '[0-9]' | tr -d '\n')
+# Restart last child project build
+curl -X POST https://api.travis-ci.org/builds/$BUILD_NUM/restart --header "Authorization: token "$AUTH_TOKEN
+
 exit $?
