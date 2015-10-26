110917a81cec9f5107341a78919fd1a44c47e7c7
nicolas-f <github@nettrader.fr>
2015-10-26 16:06:09 +0100

    add missing import

--- h2drivers/src/test/java/org/h2gis/drivers/raster/WorldImageImportExportTest.java
+++ h2drivers/src/test/java/org/h2gis/drivers/raster/WorldImageImportExportTest.java
@@ -40,6 +40,8 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import org.h2.jdbc.JdbcSQLException;
