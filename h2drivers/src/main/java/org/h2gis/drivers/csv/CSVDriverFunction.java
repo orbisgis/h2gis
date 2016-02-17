5d014a024f9cb9b1d3123a8d08ed7a6a8c1c5c49
Erwan Bocher <erwan.bocher@ec-nantes.fr>
2016-02-17 09:42:31 +0100

    Add progress monitor on the csv import driver

--- h2drivers/src/main/java/org/h2gis/drivers/csv/CSVDriverFunction.java
+++ h2drivers/src/main/java/org/h2gis/drivers/csv/CSVDriverFunction.java
@@ -22,8 +22,12 @@
  */
 package org.h2gis.drivers.csv;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.channels.FileChannel;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -103,7 +107,15 @@ public class CSVDriverFunction implements DriverFunction{
             final boolean isH2 = JDBCUtilities.isH2DataBase(connection.getMetaData());
             TableLocation requestedTable = TableLocation.parse(tableReference, isH2);
             String table = requestedTable.getTable();
-            ResultSet reader = new Csv().read(fileName.getPath(), null, null);
+            int AVERAGE_NODE_SIZE = 500;
+            FileInputStream fis = new FileInputStream(fileName);
+            FileChannel fc = fis.getChannel();
+            long fileSize = fc.size();
+            // Given the file size and an average node file size.
+            // Skip how many nodes in order to update progression at a step of 1%
+            long readFileSizeEachNode = Math.max(1, (fileSize / AVERAGE_NODE_SIZE) / 100);            
+            int nodeCountProgress = 0;
+            ResultSet reader = new Csv().read(new BufferedReader(new InputStreamReader(fis)), null);
             ResultSetMetaData metadata = reader.getMetaData();
             int columnCount = metadata.getColumnCount();
 
@@ -132,6 +144,10 @@ public class CSVDriverFunction implements DriverFunction{
             long batchSize = 0;
             try {
                 while (reader.next()) {
+                    if (progress.isCanceled()) {
+                        throw new SQLException("Canceled by user");
+                    }
+                    
                     for (int i = 0; i < columnCount; i++) {
                         pst.setString(i + 1, reader.getString(i + 1));
                     }
@@ -145,6 +161,14 @@ public class CSVDriverFunction implements DriverFunction{
                 }
                 if (batchSize > 0) {
                     pst.executeBatch();
+                }
+                if (nodeCountProgress++ % readFileSizeEachNode == 0) {
+                    // Update Progress
+                    try {
+                        progress.setStep((int) (((double) fc.position() / fileSize) * 100));
+                    } catch (IOException ex) {
+                        // Ignore
+                    }
                 }
 
             } finally {
