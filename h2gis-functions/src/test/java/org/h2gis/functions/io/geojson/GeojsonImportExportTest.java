5ef4d9f51e96f613750e566816ae59cac686c4a0
ebocher <erwan.bocher@univ-ubs.fr>
2016-08-24 10:08:34 +0200

    Remove unnecessary test

--- h2gis-functions/src/test/java/org/h2gis/functions/io/geojson/GeojsonImportExportTest.java
+++ h2gis-functions/src/test/java/org/h2gis/functions/io/geojson/GeojsonImportExportTest.java
@@ -568,13 +568,5 @@ public class GeojsonImportExportTest {
         stat.execute("DROP TABLE IF EXISTS TABLE_POINTS_READ");
         stat.close();
     }
-    
-    @Test
-    public void testRead() throws Exception {
-        Statement stat = connection.createStatement();
-        stat.execute("DROP TABLE IF EXISTS TABLE_COMPLEX_READ");
-        stat.execute("CALL GeoJsonRead("+ StringUtils.quoteStringSQL("/tmp/bigdata.geojson") + ", 'TABLE_COMPLEX_READ');");
-        ResultSet res = stat.executeQuery("SELECT * FROM TABLE_COMPLEX_READ;");
-    }
         
 }
