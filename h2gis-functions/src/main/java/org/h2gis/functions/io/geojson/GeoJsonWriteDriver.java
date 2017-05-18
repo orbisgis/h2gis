e742b2fcd406555ecf955881066875b210a4e539
Pham Hai Trung <inventeurno1@gmail.com>
2017-05-18 15:37:52 +0200

    Remove Pattern class

--- h2gis-functions/src/main/java/org/h2gis/functions/io/geojson/GeoJsonWriteDriver.java
+++ h2gis-functions/src/main/java/org/h2gis/functions/io/geojson/GeoJsonWriteDriver.java
@@ -34,7 +34,6 @@ import java.sql.*;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 import org.h2gis.functions.io.utility.FileUtil;
 
