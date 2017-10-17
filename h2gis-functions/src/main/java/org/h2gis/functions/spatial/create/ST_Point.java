c4c15fa5c26076d41610f60fe8882e8477133766
ebocher <erwan.bocher@univ-ubs.fr>
2017-10-17 08:34:49 +0200

    Add ST_POINT alias

---
+++ h2gis-functions/src/main/java/org/h2gis/functions/spatial/create/ST_Point.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.h2gis.functions.spatial.create;
+
+/**
+ *
+ * ST_Point constructs POINT from two or three doubles.
+ *
+ * @author Erwan Bocher
+ */
+public class ST_Point extends ST_MakePoint{
+    
+}
