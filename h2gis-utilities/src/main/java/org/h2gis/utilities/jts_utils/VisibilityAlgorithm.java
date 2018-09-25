d6b09ed14736011e6ebbd1b6f919e27d4c0c9250
nicolas-f <fortin@ad.ifsttar.fr>
2018-09-25 13:01:28 +0200

    clean debug code

--- h2gis-utilities/src/main/java/org/h2gis/utilities/jts_utils/VisibilityAlgorithm.java
+++ h2gis-utilities/src/main/java/org/h2gis/utilities/jts_utils/VisibilityAlgorithm.java
@@ -65,6 +65,7 @@ public class VisibilityAlgorithm {
 
   /**
    * Add occlusion segment in isovist
+   *
    * @param p0 segment origin
    * @param p1 segment destination
    */
@@ -79,17 +80,10 @@ public class VisibilityAlgorithm {
     return Math.atan2(b.y - a.y, b.x - a.x);
   }
 
-  private static int sum(List<Integer> values) {
-    int tot = 0;
-    for(int val : values) {
-      tot += val;
-    }
-    return tot;
-  }
-
   /**
    * Compute isovist polygon
-   * @param position View coordinate
+   *
+   * @param position    View coordinate
    * @param addEnvelope If true add circle bounding box. This function does not work properly if the view point is not
    *                    enclosed by segments
    * @return Visibility polygon
@@ -100,34 +94,32 @@ public class VisibilityAlgorithm {
 
     // Compute envelope
     Envelope env = new Envelope();
-    for(SegmentString segment : originalSegments) {
+    for (SegmentString segment : originalSegments) {
       env.expandToInclude(segment.getCoordinate(0));
       env.expandToInclude(segment.getCoordinate(1));
     }
-    if(addEnvelope) {
+    if (addEnvelope) {
       // Add bounding geom in envelope
-      env.expandToInclude(new Coordinate(position.x-maxDistance, position.y-maxDistance));
-      env.expandToInclude(new Coordinate(position.x+maxDistance, position.y+position.x));
+      env.expandToInclude(new Coordinate(position.x - maxDistance, position.y - maxDistance));
+      env.expandToInclude(new Coordinate(position.x + maxDistance, position.y + position.x));
       GeometricShapeFactory geometricShapeFactory = new GeometricShapeFactory();
       geometricShapeFactory.setCentre(new Coordinate(position.x - env.getMinX(), position.y - env.getMinY()));
       geometricShapeFactory.setWidth(maxDistance * 2);
       geometricShapeFactory.setHeight(maxDistance * 2);
       geometricShapeFactory.setNumPoints(numPoints);
       addPolygon(bounded, geometricShapeFactory.createEllipse());
-      for(SegmentString segment : originalSegments) {
+      for (SegmentString segment : originalSegments) {
         final Coordinate a = segment.getCoordinate(0);
         final Coordinate b = segment.getCoordinate(1);
-        addSegment(bounded, new Coordinate(a.x - env.getMinX(), a.y - env.getMinY()),
-                new Coordinate(b.x - env.getMinX(), b.y - env.getMinY()));
+        addSegment(bounded, new Coordinate(a.x - env.getMinX(), a.y - env.getMinY()), new Coordinate(b.x - env.getMinX(), b.y - env.getMinY()));
       }
       // Intersection with bounding circle
       bounded = fixSegments(bounded);
     } else {
-      for(SegmentString segment : originalSegments) {
+      for (SegmentString segment : originalSegments) {
         final Coordinate a = segment.getCoordinate(0);
         final Coordinate b = segment.getCoordinate(1);
-        addSegment(bounded, new Coordinate(a.x - env.getMinX(), a.y - env.getMinY()),
-                new Coordinate(b.x - env.getMinX(), b.y - env.getMinY()));
+        addSegment(bounded, new Coordinate(a.x - env.getMinX(), a.y - env.getMinY()), new Coordinate(b.x - env.getMinX(), b.y - env.getMinY()));
       }
     }
 
@@ -135,15 +127,11 @@ public class VisibilityAlgorithm {
 
     List<Vertex> sorted = new ArrayList<>(bounded.size() * 2);
 
-    //System.out.println(String.format(Locale.ROOT, "var observer_x = %.4f;", position.x));
-    //System.out.println(String.format(Locale.ROOT, "var observer_y = %.4f;", position.y));
-
     for (int idSegment = 0; idSegment < bounded.size(); idSegment++) {
       SegmentString segment = bounded.get(idSegment);
 
-      //System.out.println(String.format(Locale.ROOT,"segments.push([[%.2f, %.2f],[%.2f, %.2f]]);", segment.getCoordinate(0).x,segment.getCoordinate(0).y,segment.getCoordinate(1).x,segment.getCoordinate(1).y));
       // Convert segment to angle relative to viewPoint
-      for(int j=0; j < 2; ++j) {
+      for (int j = 0; j < 2; ++j) {
         final Coordinate pt = segment.getCoordinate(j);
         sorted.add(new Vertex(idSegment, j, angle(pt, position)));
       }
@@ -151,25 +139,25 @@ public class VisibilityAlgorithm {
     Collections.sort(sorted);
 
     List<Integer> map = new ArrayList<>(bounded.size());
-    for(int i=0; i<bounded.size();i++) {
+    for (int i = 0; i < bounded.size(); i++) {
       map.add(-1);
     }
     List<Integer> heap = new ArrayList<>(bounded.size());
     Coordinate start = new Coordinate(position.x + 1, position.y);
 
     // Init heap and map lists
-    for(int i=0; i < bounded.size(); i++) {
+    for (int i = 0; i < bounded.size(); i++) {
       SegmentString seg = bounded.get(i);
       double a1 = angle(seg.getCoordinate(0), position);
       double a2 = angle(seg.getCoordinate(1), position);
       boolean active = false;
-      if(a1 > -Math.PI && a1 <= 0 && a2 <= Math.PI && a2 >= 0 && a2 - a1 > Math.PI) {
+      if (a1 > -Math.PI && a1 <= 0 && a2 <= Math.PI && a2 >= 0 && a2 - a1 > Math.PI) {
         active = true;
       }
-      if(a2 > -Math.PI && a2 <= 0 && a1 <= Math.PI && a1 >= 0 && a1 - a2 > Math.PI) {
+      if (a2 > -Math.PI && a2 <= 0 && a1 <= Math.PI && a1 >= 0 && a1 - a2 > Math.PI) {
         active = true;
       }
-      if(active) {
+      if (active) {
         insert(i, heap, position, bounded, start, map);
       }
     }
@@ -177,45 +165,38 @@ public class VisibilityAlgorithm {
     List<Coordinate> polygon = new ArrayList<>();
 
     // Iterate over vertices using the anticlockwise order
-    for(int i=0; i < sorted.size();) {
+    for (int i = 0; i < sorted.size(); ) {
       boolean extend = false;       // Use existing vertex
       boolean shorten = false;      // Compute intersection with two vertices
       int orig = i;
       Coordinate vertex = bounded.get(sorted.get(i).idSegment).getCoordinate(sorted.get(i).vertexIndex);
       int oldSegment = heap.get(0);
       do {
-        int hash = 0;
-        for(int hashi = 0; hashi < heap.size(); hashi++) {
-          hash+=hashi*heap.get(hashi);
-        }
-        System.out.println(String.format("%d,%d",i,hash));
-        if(map.get(sorted.get(i).idSegment) != -1) {
-          if(sorted.get(i).idSegment == oldSegment) {
+        if (map.get(sorted.get(i).idSegment) != -1) {
+          if (sorted.get(i).idSegment == oldSegment) {
             extend = true;
             vertex = bounded.get(sorted.get(i).idSegment).getCoordinate(sorted.get(i).vertexIndex);
           }
-          //System.out.println(String.format("%d,remove,%d", i,map.get(sorted.get(i).idSegment)));
           remove(map.get(sorted.get(i).idSegment), heap, position, bounded, vertex, map);
         } else {
-          //System.out.println(String.format("%d,insert,%d", i,sorted.get(i).idSegment));
           insert(sorted.get(i).idSegment, heap, position, bounded, vertex, map);
-          if(heap.get(0) != oldSegment) {
+          if (heap.get(0) != oldSegment) {
             shorten = true;
           }
         }
         i++;
-        if(i == sorted.size()) {
+        if (i == sorted.size()) {
           break;
         }
       } while (sorted.get(i).angle < sorted.get(orig).angle + epsilon);
 
-      if(extend) {
+      if (extend) {
         polygon.add(new Coordinate(vertex.x + env.getMinX(), vertex.y + env.getMinY()));
         Coordinate cur = intersectLines(bounded.get(heap.get(0)), position, vertex);
-        if(cur != null && !cur.equals2D(vertex, epsilon)) {
+        if (cur != null && !cur.equals2D(vertex, epsilon)) {
           polygon.add(new Coordinate(cur.x + env.getMinX(), cur.y + env.getMinY()));
         }
-      } else if(shorten) {
+      } else if (shorten) {
         final Coordinate i1 = intersectLines(bounded.get(oldSegment), position, vertex);
         final Coordinate i2 = intersectLines(bounded.get(heap.get(0)), position, vertex);
         polygon.add(new Coordinate(i1.x + env.getMinX(), i1.y + env.getMinY()));
@@ -228,7 +209,7 @@ public class VisibilityAlgorithm {
     return geometryFactory.createPolygon(polygon.toArray(new Coordinate[polygon.size()]));
   }
 
-  private static Coordinate intersectLines(SegmentString a, Coordinate b1,  Coordinate b2) {
+  private static Coordinate intersectLines(SegmentString a, Coordinate b1, Coordinate b2) {
     final Coordinate a1 = a.getCoordinate(0);
     final Coordinate a2 = a.getCoordinate(1);
     double dbx = b2.x - b1.x;
@@ -236,7 +217,7 @@ public class VisibilityAlgorithm {
     double dax = a2.x - a1.x;
     double day = a2.y - a1.y;
 
-    double u_b  = dby * dax - dbx * day;
+    double u_b = dby * dax - dbx * day;
     if (u_b != 0) {
       double ua = (dbx * (a1.y - b1.y) - dby * (a1.x - b1.x)) / u_b;
       return new Coordinate(a1.x - ua * -dax, a1.y - ua * -day);
@@ -249,23 +230,23 @@ public class VisibilityAlgorithm {
   }
 
   private static int getParent(int index) {
-    return (int)Math.floor((index-1)/2.0);
+    return (int) Math.floor((index - 1) / 2.0);
   }
 
   private double angle2(Coordinate a, Coordinate b, Coordinate c) {
     double a1 = angle(a, b);
     double a2 = angle(b, c);
     double a3 = a1 - a2;
-    if(a3 < 0) {
+    if (a3 < 0) {
       a3 += M_2PI;
     }
-    if(a3 > M_2PI) {
+    if (a3 > M_2PI) {
       a3 -= M_2PI;
     }
     return a3;
   }
 
-  private boolean lessThan(int index1,int index2,Coordinate position, List<SegmentString> segments, Coordinate destination) {
+  private boolean lessThan(int index1, int index2, Coordinate position, List<SegmentString> segments, Coordinate destination) {
 
     Coordinate inter1 = intersectLines(segments.get(index1), position, destination);
     Coordinate inter2 = intersectLines(segments.get(index2), position, destination);
@@ -293,17 +274,17 @@ public class VisibilityAlgorithm {
 
   private void remove(int index, List<Integer> heap, Coordinate position, List<SegmentString> segments, Coordinate destination, List<Integer> map) {
     map.set(heap.get(index), -1);
-    if(index == heap.size() -1) {
+    if (index == heap.size() - 1) {
       heap.remove(heap.size() - 1);
       return;
     }
     heap.set(index, heap.remove(heap.size() - 1));
     map.set(heap.get(index), index);
     int cur = index;
-    if(cur != 0 && lessThan(heap.get(cur), heap.get(getParent(cur)), position, segments, destination)) {
-      while(cur > 0) {
+    if (cur != 0 && lessThan(heap.get(cur), heap.get(getParent(cur)), position, segments, destination)) {
+      while (cur > 0) {
         int parent = getParent(cur);
-        if(!lessThan(heap.get(cur), heap.get(parent), position, segments, destination)) {
+        if (!lessThan(heap.get(cur), heap.get(parent), position, segments, destination)) {
           break;
         }
         map.set(heap.get(parent), cur);
@@ -317,15 +298,14 @@ public class VisibilityAlgorithm {
       while (true) {
         int left = getChild(cur);
         int right = left + 1;
-        if(left < heap.size() && lessThan(heap.get(left), heap.get(cur), position, segments, destination) &&
-                (right == heap.size() || lessThan(heap.get(left), heap.get(right), position, segments, destination))) {
+        if (left < heap.size() && lessThan(heap.get(left), heap.get(cur), position, segments, destination) && (right == heap.size() || lessThan(heap.get(left), heap.get(right), position, segments, destination))) {
           map.set(heap.get(left), cur);
           map.set(heap.get(cur), left);
           int temp = heap.get(left);
           heap.set(left, heap.get(cur));
           heap.set(cur, temp);
           cur = left;
-        } else if(right < heap.size() && lessThan(heap.get(right), heap.get(cur), position, segments, destination)) {
+        } else if (right < heap.size() && lessThan(heap.get(right), heap.get(cur), position, segments, destination)) {
           map.set(heap.get(right), cur);
           map.set(heap.get(cur), right);
           int temp = heap.get(right);
@@ -390,19 +370,20 @@ public class VisibilityAlgorithm {
   }
 
   private static void addGeometry(List<SegmentString> segments, GeometryCollection geometry) {
-      int geoCount = geometry.getNumGeometries();
-      for (int n = 0; n < geoCount; n++) {
-        Geometry simpleGeom = geometry.getGeometryN(n);
-        if (simpleGeom instanceof LineString) {
-          addLineString(segments, (LineString) simpleGeom);
-        } else if (simpleGeom instanceof Polygon) {
-          addPolygon(segments, (Polygon)simpleGeom);
-        }
+    int geoCount = geometry.getNumGeometries();
+    for (int n = 0; n < geoCount; n++) {
+      Geometry simpleGeom = geometry.getGeometryN(n);
+      if (simpleGeom instanceof LineString) {
+        addLineString(segments, (LineString) simpleGeom);
+      } else if (simpleGeom instanceof Polygon) {
+        addPolygon(segments, (Polygon) simpleGeom);
       }
+    }
   }
 
   /**
    * Explode geometry and add occlusion segments in isovist
+   *
    * @param geometry Geometry collection, LineString or Polygon instance
    */
   public void addGeometry(Geometry geometry) {
@@ -410,7 +391,7 @@ public class VisibilityAlgorithm {
       addLineString(originalSegments, (LineString) geometry);
     } else if (geometry instanceof Polygon) {
       addPolygon(originalSegments, (Polygon) geometry);
-    } else if(geometry instanceof GeometryCollection) {
+    } else if (geometry instanceof GeometryCollection) {
       addGeometry(originalSegments, (GeometryCollection) geometry);
     }
   }
