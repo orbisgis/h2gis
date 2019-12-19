3bbf4cf511fcd3c87a4c9b96505f52cd298cc11a
Sylvain Palominos <sylvain.palominos@univ-ubs.fr>
2019-12-19 11:57:49 +0100

    Add a method to SFSUtilities to set the SRID of a column.

--- h2gis-utilities/src/main/java/org/h2gis/utilities/SFSUtilities.java
+++ h2gis-utilities/src/main/java/org/h2gis/utilities/SFSUtilities.java
@@ -603,17 +603,17 @@ public class SFSUtilities {
         }
         return new String[]{authority, sridCode};
     }
-    
-    
+
+
     /**
      * Alter a table to add a SRID constraint.
      * The srid must be greater than zero.
-     * 
+     *
      * @param connection Active connection
      * @param tableLocation TableLocation of the table to update
      * @param srid SRID to set
      *
-     * @throws SQLException 
+     * @throws SQLException
      */
     public static void addTableSRIDConstraint(Connection connection, TableLocation tableLocation, int srid)
             throws SQLException {
@@ -621,6 +621,30 @@ public class SFSUtilities {
         if (srid > 0) {
             connection.createStatement().execute(String.format("ALTER TABLE %s ADD CHECK ST_SRID(the_geom)=%d",
                     tableLocation.toString(), srid));
+        }
+    }
+
+
+    /**
+     * Alter a table to add a SRID constraint on the given column.
+     * The srid must be greater than zero.
+     *
+     * @param connection Active connection
+     * @param tableLocation TableLocation of the table to update
+     * @param columnName Name of the column to alter
+     * @param srid SRID to set
+     *
+     * @throws SQLException
+     */
+    public static void addColumnSRIDConstraint(Connection connection, TableLocation tableLocation, String columnName,
+                                              int srid)
+            throws SQLException {
+        //Alter table to set the SRID constraint
+        if (srid > 0) {
+            String formattedColumn = TableLocation.quoteIdentifier(columnName,
+                    JDBCUtilities.isH2DataBase(connection.getMetaData()));
+            connection.createStatement().execute(String.format(
+                    "ALTER TABLE %s ADD CHECK ST_SRID(" + formattedColumn + ")=%d", tableLocation.toString(), srid));
         }
     }
     
