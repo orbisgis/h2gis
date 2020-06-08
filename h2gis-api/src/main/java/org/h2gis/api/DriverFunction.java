e1d0964fe330b595b995fa8f420816e29bacaf8d
ebocher <erwan.bocher@univ-ubs.fr>
2020-06-08 16:03:18 +0200

    Fix spaces

--- h2gis-api/src/main/java/org/h2gis/api/DriverFunction.java
+++ h2gis-api/src/main/java/org/h2gis/api/DriverFunction.java
@@ -17,7 +17,6 @@
  * For more information, please consult: <http://www.h2gis.org/>
  * or contact directly: info_at_h2gis.org
  */
-
 package org.h2gis.api;
 
 import java.io.File;
@@ -26,8 +25,9 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
- * This function can import/export a file into/from a table. Connection may be on a remote H2/Postgres database.
- * The file can be linked to the database or copied into the database.
+ * This function can import/export a file into/from a table. Connection may be
+ * on a remote H2/Postgres database. The file can be linked to the database or
+ * copied into the database.
  *
  * @author Nicolas Fortin
  * @author Sylvain PALOMINOS (UBS 2018)
@@ -35,16 +35,19 @@ import java.sql.SQLException;
 public interface DriverFunction {
 
     /**
-     * A linked table is created instantly but work only if the DataBase is local.
-     * A copy will transfer Data from the File to the remote/local database and the database content will not be
-     * synced with the file.
+     * A linked table is created instantly but work only if the DataBase is
+     * local. A copy will transfer Data from the File to the remote/local
+     * database and the database content will not be synced with the file.
      */
-    enum IMPORT_DRIVER_TYPE {LINK, COPY}
+    enum IMPORT_DRIVER_TYPE {
+        LINK, COPY
+    }
 
     /**
      * Return the driver import type.
      *
-     * @return The driver type. A LINK Driver mean the usage of a TableEngine on H2 DataBase.
+     * @return The driver type. A LINK Driver mean the usage of a TableEngine on
+     * H2 DataBase.
      */
     IMPORT_DRIVER_TYPE getImportDriverType();
 
@@ -65,15 +68,16 @@ public interface DriverFunction {
     /**
      * Return the description of the specified format.
      *
-     * @param format Format given through getImportFormats and/or getExportFormats
-     * @return The description of this format under the default Locale.
-     * An empty string if the description is not available.
+     * @param format Format given through getImportFormats and/or
+     * getExportFormats
+     * @return The description of this format under the default Locale. An empty
+     * string if the description is not available.
      */
     String getFormatDescription(String format);
 
     /**
-     * Returns true if the file extension is from a spatial file, false otherwise or if the driver does not recognize
-     * it.
+     * Returns true if the file extension is from a spatial file, false
+     * otherwise or if the driver does not recognize it.
      *
      * @param extension Extension to check.
      * @return True if the extension is a spatial one, false otherwise.
@@ -81,120 +85,129 @@ public interface DriverFunction {
     boolean isSpatialFormat(String extension);
 
     /**
-     * Export the specified table from the specified connection into the specified file.
+     * Export the specified table from the specified connection into the
+     * specified file.
      *
-     * @param connection     Active connection, do not close this connection.
+     * @param connection Active connection, do not close this connection.
      * @param tableReference [[catalog.]schema.]table reference.
-     * @param fileName       File path to write, if exists it may be replaced.
-     * @param progress       Progress visitor following the execution.
+     * @param fileName File path to write, if exists it may be replaced.
+     * @param progress Progress visitor following the execution.
      * @throws SQLException Table read error.
-     * @throws IOException  File write error.
+     * @throws IOException File write error.
      */
     void exportTable(Connection connection, String tableReference, File fileName, ProgressVisitor progress)
             throws SQLException, IOException;
-    
-      /**
-     * Export the specified table from the specified connection into the specified file.
+
+    /**
+     * Export the specified table from the specified connection into the
+     * specified file.
      *
-     * @param connection     Active connection, do not close this connection.
+     * @param connection Active connection, do not close this connection.
      * @param tableReference [[catalog.]schema.]table reference.
-     * @param fileName       File path to write, if exists it may be replaced.
-     * @param deleteFiles     True to delete the files if exist
-     * @param progress       Progress visitor following the execution.
+     * @param fileName File path to write, if exists it may be replaced.
+     * @param deleteFiles True to delete the files if exist
+     * @param progress Progress visitor following the execution.
      * @throws SQLException Table read error.
-     * @throws IOException  File write error.
+     * @throws IOException File write error.
      */
     void exportTable(Connection connection, String tableReference, File fileName, boolean deleteFiles, ProgressVisitor progress)
             throws SQLException, IOException;
 
     /**
-     * Export the specified table from the specified connection into the specified file.
+     * Export the specified table from the specified connection into the
+     * specified file.
      *
-     * @param connection     Active connection, do not close this connection.
+     * @param connection Active connection, do not close this connection.
      * @param tableReference [[catalog.]schema.]table reference.
-     * @param fileName       File path to write, if exists it may be replaced.
-     * @param options        Options to use for the export like encoding, separator ...
-     *                       The options are different from a format to another.
-     * @param deleteFiles     True to delete the files if exist
-     * @param progress       Progress visitor following the execution.
+     * @param fileName File path to write, if exists it may be replaced.
+     * @param options Options to use for the export like encoding, separator ...
+     * The options are different from a format to another.
+     * @param deleteFiles True to delete the files if exist
+     * @param progress Progress visitor following the execution.
      * @throws SQLException Table read error.
-     * @throws IOException  File write error.
+     * @throws IOException File write error.
      */
     void exportTable(Connection connection, String tableReference, File fileName,
-                     String options,boolean deleteFiles,ProgressVisitor progress) throws SQLException, IOException;
-    
-    /**
-     * Export the specified table from the specified connection into the specified file.
-     *
-     * @param connection     Active connection, do not close this connection.
-     * @param tableReference [[catalog.]schema.]table reference.
-     * @param fileName       File path to write, if exists it may be replaced.
-     * @param progress       Progress visitor following the execution.
-     * @param options        Options to use for the export like encoding, separator ...
-     *                       The options are different from a format to another.
-     * @throws SQLException Table read error.
-     * @throws IOException  File write error.
-     */
-    void exportTable(Connection connection, String tableReference, File fileName,
-                     String options,ProgressVisitor progress) throws SQLException, IOException;
+            String options, boolean deleteFiles, ProgressVisitor progress) throws SQLException, IOException;
 
     /**
-     * Import the specified file into the specified table in the specified connection.
+     * Export the specified table from the specified connection into the
+     * specified file.
      *
-     * @param connection     Active connection, do not close this connection.
+     * @param connection Active connection, do not close this connection.
      * @param tableReference [[catalog.]schema.]table reference.
-     * @param fileName       File path to read.
-     * @param progress       Progress visitor following the execution.
+     * @param fileName File path to write, if exists it may be replaced.
+     * @param progress Progress visitor following the execution.
+     * @param options Options to use for the export like encoding, separator ...
+     * The options are different from a format to another.
+     * @throws SQLException Table read error.
+     * @throws IOException File write error.
+     */
+    void exportTable(Connection connection, String tableReference, File fileName,
+            String options, ProgressVisitor progress) throws SQLException, IOException;
+
+    /**
+     * Import the specified file into the specified table in the specified
+     * connection.
+     *
+     * @param connection Active connection, do not close this connection.
+     * @param tableReference [[catalog.]schema.]table reference.
+     * @param fileName File path to read.
+     * @param progress Progress visitor following the execution.
      * @throws SQLException Table write error.
-     * @throws IOException  File read error.
+     * @throws IOException File read error.
      */
     void importFile(Connection connection, String tableReference, File fileName, ProgressVisitor progress)
             throws SQLException, IOException;
 
     /**
-     * Import the specified file into the specified table in the specified connection.
+     * Import the specified file into the specified table in the specified
+     * connection.
      *
-     * @param connection     Active connection, do not close this connection.
+     * @param connection Active connection, do not close this connection.
      * @param tableReference [[catalog.]schema.]table reference.
-     * @param fileName       File path to read.
-     * @param options        Options to use for the export like encoding, separator ...
-     *                       The options are different from a format to another.
-     * @param progress       Progress visitor following the execution.
+     * @param fileName File path to read.
+     * @param options Options to use for the export like encoding, separator ...
+     * The options are different from a format to another.
+     * @param progress Progress visitor following the execution.
      * @throws SQLException Table write error.
-     * @throws IOException  File read error.
+     * @throws IOException File read error.
      */
     void importFile(Connection connection, String tableReference, File fileName, String options, ProgressVisitor progress)
-                     throws SQLException, IOException;
+            throws SQLException, IOException;
 
     /**
-     * Import the specified file into the specified table in the specified connection.
+     * Import the specified file into the specified table in the specified
+     * connection.
      *
-     * @param connection     Active connection, do not close this connection.
+     * @param connection Active connection, do not close this connection.
      * @param tableReference [[catalog.]schema.]table reference.
-     * @param fileName       File path to read.
-     * @param deleteTables   True if the existing table used for the import should be deleted, false otherwise.
-     * @param progress       Progress visitor following the execution.
+     * @param fileName File path to read.
+     * @param deleteTables True if the existing table used for the import should
+     * be deleted, false otherwise.
+     * @param progress Progress visitor following the execution.
      * @throws SQLException Table write error.
-     * @throws IOException  File read error.
+     * @throws IOException File read error.
      */
     void importFile(Connection connection, String tableReference, File fileName, boolean deleteTables, ProgressVisitor progress
-                    ) throws SQLException, IOException;
-    
+    ) throws SQLException, IOException;
+
     /**
-     * Import the specified file into the specified table in the specified connection.
+     * Import the specified file into the specified table in the specified
+     * connection.
      *
-     * @param connection     Active connection, do not close this connection.
+     * @param connection Active connection, do not close this connection.
      * @param tableReference [[catalog.]schema.]table reference.
-     * @param fileName       File path to read.
-     * @param options        Options to use for the export like encoding, separator ...
-     *                       The options are different from a format to another.
-     * @param deleteTables   True if the existing table used for the import should be deleted, false otherwise.
-     * @param progress       Progress visitor following the execution.
+     * @param fileName File path to read.
+     * @param options Options to use for the export like encoding, separator ...
+     * The options are different from a format to another.
+     * @param deleteTables True if the existing table used for the import should
+     * be deleted, false otherwise.
+     * @param progress Progress visitor following the execution.
      * @throws SQLException Table write error.
-     * @throws IOException  File read error.
+     * @throws IOException File read error.
      */
     void importFile(Connection connection, String tableReference, File fileName, String options, boolean deleteTables, ProgressVisitor progress
-                    ) throws SQLException, IOException;
-    
-    
+    ) throws SQLException, IOException;
+
 }
