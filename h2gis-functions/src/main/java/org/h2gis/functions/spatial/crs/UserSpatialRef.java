0edcbba14aea349bfdeecde9a19c735f641553d8
ebocher <erwan.bocher@univ-ubs.fr>
2022-06-16 10:02:59 +0200

    Prepare user spatial ref table

---
+++ h2gis-functions/src/main/java/org/h2gis/functions/spatial/crs/UserSpatialRef.java
@@ -0,0 +1,13 @@
+package org.h2gis.functions.spatial.crs;
+
+/**
+ * This class contains methods to manage custom CoordinateReferenceSystem that can be build
+ * from a proj file
+ *
+ */
+public class UserSpatialRef {
+
+    static void createUserSpatialRefTable(){
+
+    }
+}
