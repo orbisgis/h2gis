d832ebf9226244aa4a8075863a804a84403e08b4
ebocher <erwan.bocher@cnrs.fr>
2025-11-27 11:03:59 +0100

    Header

--- h2gis-functions/src/main/java/org/h2gis/functions/spatial/generalize/ST_SimplifyPolygonHull.java
+++ h2gis-functions/src/main/java/org/h2gis/functions/spatial/generalize/ST_SimplifyPolygonHull.java
@@ -1,3 +1,22 @@
+/**
+ * H2GIS is a library that brings spatial support to the H2 Database Engine
+ * <a href="http://www.h2database.com">http://www.h2database.com</a>. H2GIS is developed by CNRS
+ * <a href="http://www.cnrs.fr/">http://www.cnrs.fr/</a>.
+ * <p>
+ * This code is part of the H2GIS project. H2GIS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation;
+ * version 3.0 of the License.
+ * <p>
+ * H2GIS is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details <http://www.gnu.org/licenses/>.
+ * <p>
+ * <p>
+ * For more information, please consult: <a href="http://www.h2gis.org/">http://www.h2gis.org/</a>
+ * or contact directly: info_at_h2gis.org
+ */
 package org.h2gis.functions.spatial.generalize;
 
 import org.h2gis.api.DeterministicScalarFunction;
